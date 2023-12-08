f959af39947a9f6975586b7805e85773599f65a0
Hứa Đức Quân <123366641+QDevTeam@users.noreply.github.com>
2023-12-08 09:38:12 +0700

    1

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Thời khóa biểu</title>
+  <style>
+    #timetable {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    #timetable td, #timetable th {
+      border: 1px solid #ddd;
+      padding: 8px;
+      text-align: left;
+    }
+    
+    #timetable th {
+      background-color: #f2f2f2;
+      font-weight: bold;
+    }
+  </style>
+</head>
+
+<body>
+
+<h2>Thời khóa biểu lớp 12A1</h2>
+
+<table id="timetable">
+  <tr>
+    <th>Giờ học</th> 
+    <th>Thứ 2</th>
+    <th>Thứ 3</th>
+    <th>Thứ 4</th>
+    <th>Thứ 5</th>
+    <th>Thứ 6</th>
+    <th>Thứ 7</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Toán</td>
+    <td>Văn</td>
+    <td>Anh</td>
+    <td>Lý</td>
+    <td>Hóa</td>
+    <td>Sinh</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Văn</td>
+    <td>Anh</td>
+    <td>Lý</td>
+    <td>Hóa</td> 
+    <td>Sinh</td>
+    <td>Toán</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Anh</td>
+    <td>Lý</td>
+    <td>Hóa</td>
+    <td>Sinh</td>
+    <td>Toán</td>  
+    <td>Văn</td> 
+  </tr>
+</table>
+
+</body>
+</html>
